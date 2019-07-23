2d354e00a1dfd04e5e3bab6ef7c4f0e14e4af2e6
Robert Olejnik <robert.olejnik.1986@gmail.com>
2019-07-23 11:26:18 +0100

    Colours and Fonts Added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,18 @@
 module.exports = {
   theme: {
-    extend: {}
+    fontFamily: {
+      heading: ['Lilita One', 'cursive'],
+      body: ['Roboto', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        pictonBlue: '#49B6FF',
+        vistaBlue: '#779BE7',
+        richLavender: '#A480CF',
+        skyMagenta: '#D264B6',
+        wildStrawberry: '#FF499E'
+      }
+    }
   },
   variants: {},
   plugins: []
